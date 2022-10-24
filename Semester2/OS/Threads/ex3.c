73e29a5d394c6b22b00efd63ab430063c06e9cdd
Mihalescu Valentina <116385456+Mihalescu-Valentina@users.noreply.github.com>
2022-10-24 23:06:54 +0300

    ex3.c

--- Semester2/OS/Threads/ex3.c
+++ Semester2/OS/Threads/ex3.c
@@ -1,32 +1,47 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<stdio.h>
 
-//se citesc stringuri de la tastatura pana la scrierea lui x; sa se creeze n/2 threaduri unde n este numarul de stringuri citite iar fiecare thread(i) sa retina stringurile de pe pozitiile i si i+1 contopite iar vocalele sa se transforme in cifra 1 iar la final sa se adauge nr de conoane din cuvantul nou format
+int v[10]={0};
+typedef struct{
+        int value;
+        pthread_mutex_t m;
+}data;
 
+void* f(void *arg){
 
-int main(int argc, char *argv[]){
-        /* char strings[100][20];
-        //strings = (char**)malloc(sizeof(char*)*10);
-        int i=0,ok=0;
-        scanf("%s",strings[0]);
-        while(ok==0){
-        if(strcmp(strings[i],"x")==0)
-        ok=1;
-        i++;
-        scanf("%s",strings[i]);
-        }*/
+        data dt = *((data*)arg);
+        int aux = dt.value;
+        while(aux!=0){
+                pthread_mutex_lock(&dt.m);
+                v[aux%10]++;
+                pthread_mutex_unlock(&dt.m);
+                aux = aux/10;}
+        return NULL;
+}
 
-        int n=atoi(argv[1]);
-        char read [n][20];
-        for(int i=0;i<n;i++)
-                scanf("%s",read[i]);
+int main(int argc,char *argv[]){
         int i;
-        int m=n/2;
-        pthread_t t[m];
-        for(i=0;i<n/2;i++)
-                pthread_create(&t[i],NULL,f,(void*)args*)
+        pthread_t * t = malloc(sizeof(pthread_t)*(argc-1));
+        pthread_mutex_t m ;
+        data *args = malloc(sizeof(data)*(argc-1));
 
+        if(0>pthread_mutex_init(&m,NULL)){
+                perror("Error creating the mutex");
+                exit(1);}
+        for(i=0;i<argc-1;i++){
+                args[i].value=atoi(argv[i+1]);
+                args[i].m = m;
+                pthread_create(&t[i],NULL,f,(void*)&args[i]);}
+        for (i = 0; i < argc - 1;i++) {
+                if (0 > pthread_join(t[i], NULL)) {
+                        perror("Error waiting for thread");
+                         }
+        }
+        for(i=0;i<=9;i++)
+                printf("the %d element frequency is %d \n",i,v[i]);
 
+        pthread_mutex_destroy(&m);
+        free(args);
+        free(t);
         return 0;}
